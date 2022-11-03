8b4c2aedf16340452123893913d617d8cbb96da1
isukacoffee <isukacoffee4120@gmail.com>
2022-11-03 19:53:27 +0900

    factory_bot fakerの記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+      user = FactoryBot.build(:user)
+      user.nickname = ''
+      user.valid?
+      expect(user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it "emailが空では登録できない" do
+      user = FactoryBot.build(:user)
+      user.email = ''
+      user.valid?
+      expect(user.errors.full_messages).to include "Email can't be blank"
+    end
+  end
 end

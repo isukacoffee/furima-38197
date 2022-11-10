dcc117ac5997730a5eb7d7b215fd1ceb5e517ba3
isukacoffee <isukacoffee4120@gmail.com>
2022-11-10 16:27:11 +0900

    validationの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  with_options presence: true do
+    validates :list
+    validates :detail
+    validates :situation
+    validates :category
+    validates :shipping_charge
+    validates :prefecture
+    validates :days_to_ship
+    validates :selling_price
+    validates :image
+  end
+
   belongs_to :situation
   belongs_to :category
   belongs_to :shipping_charge

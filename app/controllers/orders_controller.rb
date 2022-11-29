25ff0921cbb90edb85c23f139ec64ac07c3655de
isukacoffee <isukacoffee4120@gmail.com>
2022-11-29 21:59:57 +0900

    購入画面に遷移するpath

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,12 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @purchase_shipping = PurchaseShipping.new
   end
 
-  def new
-    @purchase_shipping = Purchase_shipping.new
-  end
 
   def create
-    @purchase_shipping = Purchase_shipping.new(purchase_record_params)
+    @purchase_shipping = PurchaseShipping.new(purchase_records_params)
     if @purchase_shipping.valid?
       @purchase_shipping.save
       redirect_to root_path
@@ -20,8 +18,7 @@ class OrdersController < ApplicationController
 
     private
 
-
-  def purchase_record_params
+  def purchase_records_params
     params.require(:purchase_shipping).permit(:post_code, :prefecture, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id)
   end
 end
